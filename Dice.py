4b1a2c8732343eb8716d5b86e2e309b582087d15
JacobRuiz3415 <jacobruiz428@gmail.com>
2025-06-24 23:42:43 -0700

    add looping

--- Dice.py
+++ Dice.py
@@ -3,7 +3,7 @@ import random
 
 class Dice:
     def __init__(self):
-        number = 0
+        sides = 0
 
     def roll():
         return random.randint(1,6)
--- DiceGame.py
+++ DiceGame.py
@@ -1,21 +1,37 @@
 from Dice import Dice
 print("***Rounds Scores***")
+# Dice created
 Dice1 = Dice()
+# Dice score
 score = 0
 round = 1
 point = 0
 
+# control variable
+choice = "y"
+
 #the game given 100 rounds
-while round <= 100:
+while (choice == "y"):
+    while round <= 100:
 
-    point = Dice.roll()
-    if round % 10 == 0:
-        print()
-    else:
-        print(point, end= " ")
-        score += point
-    
-    round += 1
-#loop end
+        point = Dice.roll()
+        if round % 10 == 0:
+            print()
+        else:
+            print(point, end= " ")
+            score += point
+        
+        round += 1
+    #end loop 
+    print(f"Average Score: {score / 100}")
+    print(f"Final Score: {score}")
+    #
+    print("would you like to start again?")
+    print("Y or N", end = " ")
 
-print(f"Final Score: {score}")
+    #restarting the loop
+    choice = input()
+    score = 0
+    round = 1
+    point = 0
+
